c1c5a8053c97372e2193f7b758a46cd2ff91303f
tolu omoseyin <toluomoseyin01@gmail.com>
2021-07-23 02:30:58 +0100

    gitcommit

--- SocialMediaApplication/SocialMediaApp.Core/Settings/JwtData.cs
+++ SocialMediaApplication/SocialMediaApp.Core/Settings/JwtData.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace SocialMediaApp.Core.Settings
 {
-    class JwtData
+    public class JwtData
     {
+        public const string Data = "JWTConfigurations";
+        public TimeSpan TokenLifeTime { get; set; }
+
+        public string SecretKey { get; set; }
+
+        public string Issuer { get; set; }
+
+        public string Audience { get; set; }
     }
 }
